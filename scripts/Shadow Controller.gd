6ecd509a26a36729feee7f31e35d261c9dd3f86d
Harmoen <harmon.the.emu@gmail.com>
2024-07-24 01:31:20 -0500

    Skewed Shadow based on light position
    There is a lot of stuff I added for debugging that can probably be removed now

--- scripts/Shadow Controller.gd
+++ scripts/Shadow Controller.gd
@@ -1,15 +1,39 @@
 extends Node2D
 
-@onready var point_light_2d = $PointLight2D
+@onready var point_light_2d := $PointLight2D
+@onready var line_a := $LineA
+@onready var line_b := $LineB
+@onready var line_c := $LineC
+@onready var test_box := get_tree().get_first_node_in_group("boxes")
 
-func _process(_delta) -> void:
-	for box in get_tree().get_nodes_in_group("boxes"):
-		box.shadow.scale.y = (box.position.y - point_light_2d.position.y) * 0.0003
-		
-		var delta_x = point_light_2d.position.x - box.position.x
-		var delta_y = point_light_2d.position.y - box.position.y
+var follow : bool = true
 
 
 func _input(event) -> void:
+	if event.is_action_pressed("Right_Mouse"):
+		follow = !follow
+	
 	if event.is_action_pressed("Left_Mouse"):
-		point_light_2d.position = get_global_mouse_position()
+		for box in get_tree().get_nodes_in_group("boxes"):
+			print(box.get_name()," > ",round(box.shadow.skew),"° > A: ",round(box.position.y - point_light_2d.position.y)," > B: ",round(box.position.x - point_light_2d.position.x))
+			print("Box: ",box.position)
+			print("Light: ", point_light_2d.position)
+		print("\n")
+	
+	if event is InputEventMouseMotion:
+		for box in get_tree().get_nodes_in_group("boxes"):
+			box.shadow.scale.y = (box.position.y - point_light_2d.position.y) * 0.002
+			var side_b : float = box.position.x - point_light_2d.position.x
+			var side_a : float = box.position.y - point_light_2d.position.y 
+			
+			
+			box.shadow.skew = atan(side_b / side_a) * -1
+		
+		if follow:
+			point_light_2d.position = get_global_mouse_position()
+			line_a.set_point_position(0,test_box.position)
+			line_a.set_point_position(1,Vector2(test_box.position.x,get_global_mouse_position().y))
+			line_b.set_point_position(0,Vector2(test_box.position.x,get_global_mouse_position().y))
+			line_b.set_point_position(1,get_global_mouse_position())
+			line_c.set_point_position(0,test_box.position)
+			line_c.set_point_position(1,get_global_mouse_position())
